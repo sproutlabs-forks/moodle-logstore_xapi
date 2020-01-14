ed39a4240eeab5836947885f6ebae74aa6e7f059
Bikram Kawan <bikramkawan@gmail.com>
2020-01-14 14:46:22 +1100

    update the version

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = isset($plugin) && is_object($plugin) ? $plugin : new \stdClass();
 $plugin->component = 'logstore_xapi';
-$plugin->version = 2019101100;
+$plugin->version = 2019101200;
 $plugin->release = '';
 $plugin->requires = 2014111000;
 $plugin->maturity = MATURITY_STABLE;
