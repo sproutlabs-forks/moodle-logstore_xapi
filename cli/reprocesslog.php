30421b4ec5ebede4efa04e2f8d424fbfd1b2ca18
Bikram Kawan <bikramkawan@gmail.com>
2023-04-03 14:58:00 +1000

    add for reprocessing for completion only

--- cli/reprocesslog.php
+++ cli/reprocesslog.php
@@ -19,21 +19,29 @@ foreach ($affectedusers as $affecteduser){
     $email = $affecteduser->Email;
     $courseidnumber = $affecteduser->Code;
 
+
     $user = $DB->get_record('user',['email'=>$email]);
   
    if($user && isset($user->id)){
-       print_r($user->id);   
+       print_r($user->id."    ");   
    }
     $course = $DB->get_record('course',['idnumber'=>$courseidnumber]);
     if($course && isset($course->id)){
-        print_r($course->id);
+        print_r($course->id."    ");
     }
+    $logs = '';
     if($user && $course){
-        $logs = $DB->get_records_sql("SELECT * from {logstore_standard_log} WHERE relateduserid={$user->id} AND courseid = {$course->id} AND( target = 'course_module_completion' OR target = 'course_completion' )");
+        if($affecteduser->CourseCompletionOnly){
+            $logs = $DB->get_records_sql("SELECT * from {logstore_standard_log} WHERE relateduserid={$user->id} AND courseid = {$course->id} AND(objecttable = 'course_completions' )");
+            
+        }else{
+            $logs = $DB->get_records_sql("SELECT * from {logstore_standard_log} WHERE relateduserid={$user->id} AND courseid = {$course->id} AND( objecttable = 'course_module_completion' OR objecttable = 'course_completion' )");
 
+        }
         if($logs){
             $DB->insert_records('logstore_xapi_log',$logs);
         }
+        print_r("Count  ".count($logs)."    ");
         print_r($email.PHP_EOL);
     }
    
